42205739d4154ea525c639e049802afb1cccc92d
Mohammad <131454582+MohammadAliMehri@users.noreply.github.com>
2025-12-20 02:27:34 +0330

    nothing

--- reverse_number_task2_point.cpp
+++ reverse_number_task2_point.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 #define NEWLINE '\n'
-int main() {
+int main()
+{
     int x;
     cout << "give me a number to reverse it : ";
     cin >> x;
 
     int reverse = 0; // sefre choon hanooz adad nadrim
-    while (x > 0) { // ta vaghti adad bozorg tar az sefr ast ejra mishe
-        int digit = x % 10; // gereftane akharin ragham
+    while (x > 0)
+    {                                   // ta vaghti adad hast
+        int digit = x % 10;             // gereftane akharin ragham
         reverse = reverse * 10 + digit; // ezafe kardan ragham be reverse
-        x /= 10; // hazf kardan akharin ragham az adad
+        x /= 10;                        // hazf kardan akharin ragham az adad
     }
 
-    cout << "Reverse --> " << reverse << NEWLINE; 
-    return 0; 
+    cout << "Reverse --> " << reverse << NEWLINE;
+    return 0;
 }
