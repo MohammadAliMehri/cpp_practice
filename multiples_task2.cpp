0ab39f8a17984f03a238b047f0f96884bf9ed610
Mohammad <131454582+MohammadAliMehri@users.noreply.github.com>
2025-12-20 01:52:29 +0330

    add some comment

--- multiples_task2.cpp
+++ multiples_task2.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 #define NEWLINE '\n'
-int main() {
+int main()
+{
     int number;
     cout << "ye adad kamtar az 10 vared konid: ";
     cin >> number;
 
-    if (number >= 10) {
-        cout << "Error!!" << NEWLINE;
+    if (number > 10) // check mikonim ke adad kamtar az 10 bashe 
+    {
+        cout << "Error!!" << NEWLINE; // payam khata
         return 1;
     }
 
-    cout << "mazhabad az " << number << " ta 100:" << NEWLINE;
-    for (int a = number; a <= 100; a += number) {
-        cout << a << " ";
+    cout << " mazrab haaye " << number << " | az 0" << " ta 100:" << NEWLINE; // payam
+    for (int a = number /* az khod number shoro mikone a */; a <= 100 /* ta vaqti kochik ta mosavi 100 bashe hesab mikone*/; a = a + number)
+    {                     // halghe baraye hesab kardan mazrabha
+        cout << a << " "; // chap kardan har mazrab
     }
     return 0;
-} 
+}
